ef2288f1d7ef25b265daa7a769273eef4e0cff5a
Joni Laatikainen <jonilaatikaine@gmail.com>
2023-01-07 15:29:05 +0200

    Created more tests for coordinates

--- src/components/Ship.test.ts
+++ src/components/Ship.test.ts
@@ -4,37 +4,37 @@ import { render } from '@testing-library/react';
 describe("Creating ships with factory function", () => {
     describe("When creating ship with length of 4", () => {
         it("Should return a ship object with length of 4", () => {
-            const ship = Ship(4);
+            const ship = Ship(4, 0, 0, "x");
             expect(ship.length).toBe(4);
         })
         it("Should return a ship object with hits of 0", () => {
-            const ship = Ship(4);
+            const ship = Ship(4, 0, 0, "x");
             expect(ship.hits).toBe(0);
         })
         it("Should return a ship object with sunken of false", () => {
-            const ship = Ship(4);
+            const ship = Ship(4, 0, 0, "x");
             expect(ship.sunken).toBe(false);
         })
         it("Calling hit() should increase hits by 1", () => {
-            const ship = Ship(2);
+            const ship = Ship(2, 0, 0, "x");
             ship.hit();
             expect(ship.hits).toBe(1);
         })
         it("Calling hit() twice should increase hits by 2", () => {
-            const ship = Ship(2);
+            const ship = Ship(2, 0, 0, "x");
             ship.hit();
             ship.hit();
             expect(ship.hits).toBe(2);
         })
         it("Calling hit() three times should increase hits by 3", () => {
-            const ship = Ship(2);
+            const ship = Ship(2, 0, 0, "x");
             ship.hit();
             ship.hit();
             ship.hit();
             expect(ship.hits).toBe(3);
         })
         it("Calling hit() four times should increase hits by 4", () => {
-            const ship = Ship(2);
+            const ship = Ship(2, 0, 0, "x");
             ship.hit();
             ship.hit();
             ship.hit();
@@ -42,11 +42,11 @@ describe("Creating ships with factory function", () => {
             expect(ship.hits).toBe(4);
         })
         it("Calling isSunk() should return false", () => {
-            const ship = Ship(2);
+            const ship = Ship(2, 0, 0, "x");
             expect(ship.isSunk()).toBe(false);
         })
         it("Calling isSunk() after 4 x hit():s should return true", () => {
-            const ship = Ship(4);
+            const ship = Ship(4, 0, 0, "x");
             ship.hit();
             ship.hit();
             ship.hit();
